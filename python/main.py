bbc77fef907f9939e5e447a4bac4697b86150e3f
benny <benny.nottonson01@student.csulb.edu>
2023-07-04 19:28:41 -0700

    fixed

--- python/main.py
+++ python/main.py
@@ -6,16 +6,20 @@ from PIL import Image, ImageFilter
 from httpx import Client
 
 url: str = argv[1]
-quantized_level = 24
+quantized_level = 32
+size = 32
+blur = 1
 
 client = Client()
 
-def get_image_from_url(url: str) -> ndarray:
+def get_image_from_url(url: str) -> Image:
     """Gets an image from a URL and converts it to rgb"""
     response_data = client.get(url, timeout=5)
-    pil_image: Image = Image.open(BytesIO(response_data.content), mode="r").convert("RGB").filter(ImageFilter.GaussianBlur(1)).quantize(quantized_level).resize((16, 16))
-    rgb_image: ndarray = array(pil_image)
-    return rgb_image
+    pil_image: Image = Image.open(BytesIO(response_data.content), mode="r").convert("RGB")
+    pil_image = pil_image.filter(ImageFilter.GaussianBlur(blur))
+    pil_image = pil_image.resize((size, size))
+    pil_image = pil_image.quantize(quantized_level)
+    return pil_image
 
 def blob_extract(mac_image: ndarray) -> tuple[int, ndarray]:
     """Extracts blobs from a quantized image"""
@@ -27,11 +31,12 @@ def blob_extract(mac_image: ndarray) -> tuple[int, ndarray]:
     return n_blobs, blob
 
 if __name__ == "__main__":
-    image: ndarray = get_image_from_url(url)
-    size_threshold = round(0.01 * image.shape[0] * image.shape[1])
-    n_blobs, blob = blob_extract(array(image))
+    image: Image = get_image_from_url(url)
+    image_array = array(image)
+    size_threshold = round(0.01 * size * size)
+    n_blobs, blob = blob_extract(image_array)
     table = [
-        [image[i][j], table[blob[i][j] - 1][1] + 1] if blob[i][j] != 0 else [0, 0]
+        [image_array[i][j], table[blob[i][j] - 1][1] + 1] if blob[i][j] != 0 else [0, 0]
         for i in range(blob.shape[0])
         for j in range(blob.shape[1])
         for table in [[[0, 0] for _ in range(0, n_blobs)]]
